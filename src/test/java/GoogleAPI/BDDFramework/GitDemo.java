d9cc497bba71b37d080d09fe18ac90c4fc138c84
Bapu Rane <bapurane41096@gmail.com>
2021-08-03 22:01:07 +0530

    this commit done by asian timezone person

---
+++ src/test/java/GoogleAPI/BDDFramework/GitDemo.java
@@ -0,0 +1,9 @@
+package GoogleAPI.BDDFramework;
+
+public class GitDemo {
+public static void main(String[] args) {
+	System.out.println("post1");
+	System.out.println("post2");
+	System.out.println("post3");
+}
+}
